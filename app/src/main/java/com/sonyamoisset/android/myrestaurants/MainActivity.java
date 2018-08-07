2bd051e0e6367a92cc82635cc7e3493fcdb77118
smoisset <smoisset@worldremit.com>
2018-08-07 15:15:38 +0100

    Initial commit

---
+++ app/src/main/java/com/sonyamoisset/android/myrestaurants/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sonyamoisset.android.myrestaurants;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLocationEditText = findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = findViewById(R.id.findRestaurantsButton);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}

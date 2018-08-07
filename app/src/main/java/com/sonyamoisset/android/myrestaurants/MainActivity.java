e68fda773e8cf691d6d870804e10b4b290aecaed
smoisset <smoisset@worldremit.com>
2018-08-07 15:36:45 +0100

    Add Ostrich fonts

--- app/src/main/java/com/sonyamoisset/android/myrestaurants/MainActivity.java
+++ app/src/main/java/com/sonyamoisset/android/myrestaurants/MainActivity.java
@@ -1,16 +1,19 @@
 package com.sonyamoisset.android.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mLocationEditText = findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = findViewById(R.id.findRestaurantsButton);
 
+        mAppNameTextView = findViewById(R.id.appNameTextView);
+        Typeface ostrichFont =
+                Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+
+        mFindRestaurantsButton = findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
 
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                Intent intent =
+                        new Intent(MainActivity.this, RestaurantsActivity.class);
                 intent.putExtra("location", location);
                 startActivity(intent);
             }
